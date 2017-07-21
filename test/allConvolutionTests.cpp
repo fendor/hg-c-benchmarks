2a6c0bfa2959a1545b5b56045334a21c83a0c163
Hannes Siebenhandl <e1327006@student.tuwien.ac.at>
2017-07-21 12:33:04 +0200

    Refactor test structure

---
+++ test/allConvolutionTests.cpp
@@ -0,0 +1,10 @@
+//
+// Created by baldr on 7/21/17.
+//
+#include "utilTests.cpp"
+#include "convolutionUtilTests.cpp"
+#include "convolutionTests.cpp"
+
+TEST(general, success) {
+    ASSERT_TRUE(1);
+}
---
+++ test/allNbodyTests.cpp
@@ -0,0 +1,10 @@
+//
+// Created by baldr on 7/21/17.
+//
+
+#include "utilTests.cpp"
+#include "nbodyTests.cpp"
+
+TEST(general, success) {
+    ASSERT_TRUE(1);
+}
